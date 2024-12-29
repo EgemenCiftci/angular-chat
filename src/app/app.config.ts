871d17f0f36c26bf4aef302c99d1a1c90530f76e
Egemen Çiftci <egemenciftci@gmail.com>
2024-12-29 19:47:17 +0100

    .

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+//import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,5 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideFirebaseApp(() => initializeApp({ "projectId": "chat-play-live", "appId": "1:597013068918:web:63e02a62392164e2097357", "storageBucket": "chat-play-live.appspot.com", "apiKey": "AIzaSyBBcjZtLKLv65_17YLE8VJMB5BzfTeSRtA", "authDomain": "chat-play-live.firebaseapp.com", "messagingSenderId": "597013068918", "measurementId": "G-2EDPSL541H" })),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()), provideAnimationsAsync()]
+    //provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideAnimationsAsync()]
 };
