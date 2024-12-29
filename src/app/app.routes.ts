53fbe2e4dd3c67d970e2378699a44168367825df
Egemen Çiftci <egemenciftci@gmail.com>
2024-12-29 20:04:53 +0100

    ng generate @angular/core:route-lazy-loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { ChatComponent } from './components/chat/chat.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+
+
 import { inject } from '@angular/core';
 import { Auth, onAuthStateChanged } from '@angular/fire/auth';
 
@@ -34,8 +34,8 @@ export const canActivate: CanActivateFn = (route: ActivatedRouteSnapshot, state:
 
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent, canActivate: [canActivate] },
-    { path: 'chat', component: ChatComponent, canActivate: [canActivate] },
+    { path: 'login', loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent), canActivate: [canActivate] },
+    { path: 'chat', loadComponent: () => import('./components/chat/chat.component').then(m => m.ChatComponent), canActivate: [canActivate] },
     { path: '', redirectTo: '/login', pathMatch: 'full' },
-    { path: '**', component: PageNotFoundComponent },
+    { path: '**', loadComponent: () => import('./components/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent) },
 ];
